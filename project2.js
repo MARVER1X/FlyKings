f38a64c8b51a26c72f160140756f23d53e866f5f
MARVER1X <126921221+MARVER1X@users.noreply.github.com>
2024-04-02 14:44:05 +0100

    Update project2.js

--- project2.js
+++ project2.js
@@ -9,18 +9,18 @@ const li7 = document.querySelector(".li7");
 const li8 = document.querySelector(".li8");
 
 //animation script for nav bar
-function navAnimator() {
-    li1.style.transition = li8.style.transition = '1.2s ease-out 0.5s';
+document.addEventListener('DOMContentLoaded', function() {
+    li1.style.transition = li8.style.transition = '1.2s ease-out 5s';
     li1.style.transform = li8.style.transform = 'translateX(0%)';
-    li2.style.transition = li7.style.transition = '0.9s ease-out 0.5s';
+    li2.style.transition = li7.style.transition = '0.9s ease-out 5s';
     li2.style.transform = li7.style.transform = 'translateX(0%)';
-    li3.style.transition = li6.style.transition = '0.6s ease-in 0.5s';
+    li3.style.transition = li6.style.transition = '0.6s ease-in 5s';
     li3.style.transform = li6.style.transform = 'translateX(0%)';
-    li4.style.transition = '0.3s ease-in 0.5s';
+    li4.style.transition = '0.3s ease-in 5s';
     li4.style.transform = 'translateX(0%)';
-    li5.style.transition = '0.3s ease-in 0.5s';
+    li5.style.transition = '0.3s ease-in 5s';
     li5.style.transform = 'translateX(0%)';
     li1.style.opacity = li2.style.opacity = li3.style.opacity = li4.style.opacity = li5.style.opacity = 
     li6.style.opacity = li7.style.opacity = li8.style.opacity = '100%';
-}
-navAnimator();
+    console.log('e suppose work bro');
+});
